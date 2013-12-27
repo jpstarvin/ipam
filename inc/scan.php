dd8a523b0189e57d5928a63aa7e24068ccde045d
jpstarvin <jpstarvin@gmail.com>
2013-12-27 08:32:29 -0600

    Update scan.php
    Moved the PDO prepared statement outside the loop and added logic to check for empty scan arguments.

--- inc/scan.php
+++ inc/scan.php
@@ -6,45 +6,40 @@
  *******************************************/
  include('/var/www/ipam/config.php');
  include('/var/www/ipam/models/dbModel.php');
- 
+
  $network = $argv[1];
  $exclusion = $argv[2];
  $snmp = $argv[3];
  $netid = $argv[4];
- 
- $cmd = "-n " . $network;
+
+$cmd = "-n " . $network;
  if($exclusion <> "none"){
- 	$cmd .= " -x " . $exclusion;
+        $cmd .= " -x " . $exclusion;
  }
  if($snmp <> "none"){
- 	$cmd .= " -s " . $snmp;
+        $cmd .= " -s " . $snmp;
  }
- 
+
  $str=exec("python inc/scan.py $cmd",$output,$ret_code);
- 
+
+$qry = $dbh->prepare("INSERT INTO ipaddress (`ipaddress`,`devicename`,`netid`) VALUES (?,?,?)");
+
  foreach ($output as $res){
- 	$out = explode(",",$res);
-	$sql = 'INSERT INTO ipaddress (';
-	if (trim($out[1]) == 'Up'){
-		$sql .= '`ipaddress`';
-		$values = ' VALUES (?';
-		$data = array(trim($out[0]));
-		if(trim($out[2]) <>''){
-			$sql .= ',`devicename`';
-			$values .= ',?';
-			array_push($data,trim($out[2]));
-		}elseif(trim($out[3]) <> ''){
-			$sql .= ',`devicename`';
-			$values .= ',?';
-			array_push($data,trim($out[3]));
-		}
-		$sql .= ',`netid`) ' . $values . ',?)';
-		array_push($data,$netid);
-		print_r($data);
-		echo $sql;
-		addRecord($dbh,$sql,$data);
-		
-	}
+        $data = NULL;
+        $out = explode(",",$res);
+        if (trim($out[1]) == 'Up'){
+                $data = array(trim($out[0]));
+                if(trim($out[2]) <>''){
+                        array_push($data,trim($out[2]));
+                }elseif(trim($out[3]) <> ''){
+                        array_push($data,trim($out[3]));
+                }else{
+                        array_push($data,"");
+                }
+                array_push($data,$netid);
+                $qry->execute($data);
+        }
  }
 
  ?>
+
